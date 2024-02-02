44bcbca9a364c396f8ae646b77985e37ad7d20ab
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2024-02-02 15:38:50 -0300

    Add linters

---
+++ src/Greeting.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+const Greeting = () => {
+  const [randomGreeting, setRandomGreeting] = useState('');
+
+  useEffect(() => {
+    const fetchRandomGreeting = async () => {
+      try {
+        // Fetch the random greeting from the Rails API
+        const response = await fetch('http://127.0.0.1:3000/random_greeting');
+        const data = await response.json();
+
+        // Set the random greeting in the state
+        setRandomGreeting(data.greeting);
+      } catch (error) {
+        console.error('Error fetching random greeting:', error);
+      }
+    };
+
+    // Call the fetch function when the component mounts
+    fetchRandomGreeting();
+  }, []); // The empty dependency array ensures this effect runs only once when the component mounts
+
+  return (
+    <div>
+      <h1>{randomGreeting}</h1>
+    </div>
+  );
+};
+
+export default Greeting;
---
+++ src/redux/reducers.js
@@ -0,0 +1,39 @@
+import {
+  FETCH_RANDOM_GREETING_REQUEST,
+  FETCH_RANDOM_GREETING_SUCCESS,
+  FETCH_RANDOM_GREETING_FAILURE,
+} from './actions';
+
+const initialState = {
+  randomGreeting: '',
+  loading: false,
+  error: null,
+};
+
+const greetingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_RANDOM_GREETING_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case FETCH_RANDOM_GREETING_SUCCESS:
+      return {
+        ...state,
+        randomGreeting: action.payload,
+        loading: false,
+        error: null,
+      };
+    case FETCH_RANDOM_GREETING_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default greetingReducer;
