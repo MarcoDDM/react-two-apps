e49ff77d68c64ea591ebc69d73f3d5619dd097bd
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2024-02-02 18:44:13 -0300

    Update App.js to wrap it inside store provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Greeting from './Greeting';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Greeting />} />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
